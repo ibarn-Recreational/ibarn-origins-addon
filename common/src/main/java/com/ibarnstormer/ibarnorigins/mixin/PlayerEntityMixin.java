e79994be4e901ec7b9fa64e1d425aa35ccdef082
ibarnstormer <48729319+ibarnstormer@users.noreply.github.com>
2025-05-11 13:59:08 -0400

    Fixed Soul Mage vegetable experience applying to some meat items

--- common/src/main/java/com/ibarnstormer/ibarnorigins/mixin/PlayerEntityMixin.java
+++ common/src/main/java/com/ibarnstormer/ibarnorigins/mixin/PlayerEntityMixin.java
@@ -3,6 +3,7 @@ package com.ibarnstormer.ibarnorigins.mixin;
 import com.ibarnstormer.ibarnorigins.entity.IbarnOriginsEntity;
 import net.minecraft.entity.ExperienceOrbEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.Recipe;
@@ -30,8 +31,9 @@ public class PlayerEntityMixin {
                 List<Recipe<?>> recipes = world.getRecipeManager().values().stream().filter(r -> r.createIcon().getItem() == stack.getItem()).toList();
                 TagKey<Item> meatTag = TagKey.of(RegistryKeys.ITEM, Identifier.of("origins", "meat"));
                 boolean hasMeatInRecipe = recipes.stream().anyMatch(r -> r.getIngredients().stream().anyMatch(i -> Arrays.stream(i.getMatchingStacks()).anyMatch(is -> is.isIn(meatTag))));
+                FoodComponent food = stack.getItem().getFoodComponent();
 
-                if (!stack.isIn(meatTag) && !hasMeatInRecipe && !world.isClient() && stack.getItem().getFoodComponent() != null) {
+                if (!stack.isIn(meatTag) && !hasMeatInRecipe && !(food != null && food.isMeat()) && !world.isClient() && stack.getItem().getFoodComponent() != null) {
                     int amount = Math.min((stack.getItem().getFoodComponent().getHunger() + Math.round(stack.getItem().getFoodComponent().getSaturationModifier())) * 2, Integer.MAX_VALUE);
                     ExperienceOrbEntity orb = new ExperienceOrbEntity(world, player.getX(), player.getY(), player.getZ(), amount);
                     world.spawnEntity(orb);
